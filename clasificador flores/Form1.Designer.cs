301f1c017fa84032e5fce980e2806247c7185674
blaster <blasterdaxx2208@gmail.com>
2020-12-22 10:44:47 -0600

    Agregar archivos de proyecto.

---
+++ clasificador flores/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace clasificador_flores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Leer Archivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(245, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(230, 500);
+            this.listBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 113);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(760, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(493, 500);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 73);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Perceptron";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 425);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(507, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(230, 500);
+            this.listBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1265, 575);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ clasificador flores/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace clasificador_flores
+{
+    public partial class Form1 : Form
+    {
+
+        string path;
+        string linea;
+        string[] secciones;
+        string[] flores = { "setosa-versicolor", "virginica-versicolor", "setosa-virginica" };
+        int contador = 0;
+        int filas = 0;
+        StreamReader txt;
+        List<string> opciones = new List<string> { "Iris-setosa", "Iris-versicolor", "Iris-virginica" };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(flores);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    this.path = openFileDialog.FileName;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+            txt = File.OpenText(path);
+            Console.WriteLine(txt.GetType());
+
+            while((linea=txt.ReadLine())!=null)
+            {
+                secciones = linea.Split(',');
+                string list="";
+                for(int i=0;i<secciones.Length;i++)
+                {
+                    list += secciones[i] + " ";
+                }
+                listBox1.Items.Add(list);
+                
+
+
+            }
+
+            txt.Close();
+                
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            columnas();
+            salidas();
+
+
+            if (comboBox1.SelectedItem.ToString().Equals(flores[0]))
+            {
+                data(opciones[0], 0);
+                data(opciones[1], 1);
+            
+            
+            }
+
+
+            if (comboBox1.SelectedItem.ToString().Equals(flores[1]))
+            {
+                data(opciones[1], 0);
+                data(opciones[2], 1);
+
+
+            }
+
+
+            if (comboBox1.SelectedItem.ToString().Equals(flores[2]))
+            {
+                data(opciones[0], 0);
+                data(opciones[2], 1);
+
+
+            }
+            contador++;
+
+
+
+
+
+
+
+
+        }
+
+        public void columnas()
+        {
+            for (int i = 1; i <=4; i++)
+            {
+                DataGridViewTextBoxColumn entradas = new DataGridViewTextBoxColumn();
+                entradas.HeaderText = "X" + i.ToString();
+                entradas.Width = 50;
+                dataGridView1.Columns.Add(entradas);
+
+
+
+            }
+        }
+
+        public void salidas()
+        {
+            DataGridViewTextBoxColumn salidaEsperada = new DataGridViewTextBoxColumn();
+            salidaEsperada.HeaderText = "Yesp";
+            salidaEsperada.Width = 50;
+            dataGridView1.Columns.Add(salidaEsperada);
+
+            DataGridViewTextBoxColumn salidaCalculada = new DataGridViewTextBoxColumn();
+            salidaCalculada.HeaderText = "Ycalc";
+            salidaCalculada.Width = 50;
+            dataGridView1.Columns.Add(salidaCalculada);
+
+        }
+
+        public void data(string flor,int i)
+        {
+
+            Console.WriteLine("entrando a data");
+            txt = File.OpenText(this.path);
+            Console.WriteLine("Agregada primera opcion");
+            int k = 0;
+            
+            Console.WriteLine(flor[i]);
+            while ((linea = this.txt.ReadLine()) != null)
+            {
+                secciones = linea.Split(',');
+                Console.WriteLine("Leyendo linea");
+
+                if (secciones.Contains(flor))
+                {
+                    Console.WriteLine("Escribiendo");
+                    dataGridView1.Rows.Add();
+                    if(i==1)
+                    {
+                        k += 50;
+                    }
+                    for (int j = 0; j < secciones.Length; j++)
+                    {
+                        this.dataGridView1.Rows[k].Cells[j].Value = (secciones[j]);
+                        
+                        
+
+                    }
+
+                    if(i==0)
+                    {
+                        this.dataGridView1.Rows[k].Cells[4].Value = "1.0";
+                    }
+                    else
+                    {
+                        this.dataGridView1.Rows[k].Cells[4].Value = "-1.0";
+                    }
+                    if (i == 1)
+                    {
+                        k -= 50;
+                    }
+                    k++;
+                    filas++;
+                }
+
+                Console.WriteLine(filas);
+                    
+
+
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(contador==0)
+            {
+                label1.Text = "Opcion no seleccionada";
+            }
+            else
+            {
+                var pesos = primeros_pesos(4);
+                listBox2.Items.Clear();
+                System.Console.WriteLine("\n");
+                var entradas = x(4);
+                string valor;
+                perceptron(4, entradas, pesos);
+            }
+        }
+
+        public static float[] primeros_pesos(int no_entradas)
+        {
+            
+            Random random = new Random();
+            float[] array = new float[no_entradas + 1];
+
+            for (int i = 0; i <= no_entradas; i++)
+            {
+                array[i] = (float)(random.Next(0, 10) / 10.0);
+
+            }
+
+            return array;
+        }
+
+
+        public float[,] x(int no_entradas)
+        {
+            float[,] entradas = new float[100, no_entradas + 1];
+            string valor;
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j <= no_entradas; j++)
+                {
+                    valor = this.dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    entradas[i, j] = float.Parse(valor);
+
+                }
+
+            }
+
+
+            return entradas;
+
+        }
+
+        public void perceptron(int no_entradas, float[,] entradas, float[] pesos)
+        {
+            float acumulador = 0;
+            int contador = 0;
+            float resultado = 0;
+            bool flag = true;
+            while (contador < 500)
+            {
+                listBox2.Items.Add("Epoca: " + contador.ToString());
+                listBox2.Items.Add("\n\n");
+                listBox2.Items.Add("W1= " + pesos[0].ToString());
+                listBox2.Items.Add("W2= " + pesos[1].ToString());
+                listBox2.Items.Add("W3= " + pesos[2].ToString());
+                listBox2.Items.Add("W4= " + pesos[3].ToString());
+                
+                listBox2.Items.Add("Umbral= " + pesos[4].ToString());
+                listBox2.Items.Add("\n");
+                for (int i = 0; i < 100; i++)
+                {
+                    acumulador = 0;
+                    
+                    
+                    
+
+                    for (int j = 0; j <= no_entradas; j++)
+                    {
+
+                        acumulador += pesos[j] * (float)(entradas[i, j]);
+                    }
+
+                    if (acumulador > 0)
+                    {
+                        resultado = 1;
+                        dataGridView1.Rows[i].Cells[no_entradas + 1].Value = resultado.ToString();
+                    }
+                    else
+                    {
+                        resultado = -1;
+                        dataGridView1.Rows[i].Cells[no_entradas + 1].Value = resultado.ToString();
+                    }
+
+                    if (resultado != entradas[i, no_entradas])
+                    {
+                        pesos = aprendizaje(no_entradas, i, pesos, entradas);
+                        Console.WriteLine("Mal clasificado");
+                        flag = false;
+                    }
+
+
+
+                }
+                contador++;
+                if (flag)
+                {
+                    
+                    break;
+                }
+                flag = true;
+            }
+
+        }
+
+        public float[] aprendizaje(int no_entradas, int index, float[] pesos, float[,] entradas)
+        {
+            float delta = 0;
+            for (int i = 0; i < no_entradas; i++)
+            {
+                delta = 0;
+                delta = entradas[index, i] * entradas[index, no_entradas];
+                pesos[i] = delta + pesos[i];
+            }
+
+            pesos[no_entradas] = entradas[index, no_entradas] + pesos[no_entradas];
+            return pesos;
+        }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+
+        }
+    }
+}
+
